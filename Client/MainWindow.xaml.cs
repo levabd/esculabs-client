fb30a405adc19fc859d9ad57104694f7569f1916
Alexey Popov <antrille@yandex.ru>
2015-09-02 20:07:26 +0600

    Date Filter

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace Client
     using MongoDB.Driver.Linq;
     using MongoRepository;
     using System.Data.Entity;
+    using System.Windows.Controls.Primitives;
     using Repo;
 
     /// <summary>
@@ -30,9 +31,56 @@ namespace Client
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string NameSearchText = "Поиск по ФИО...";
+        private const string DateSearchText = "Поиск по дате последнего сканирования...";
+
+        private List<TablePatient> patients = new List<TablePatient> { };
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void RefreshFilter()
+        {
+            IEnumerable<TablePatient> list = patients;
+            string name = nameFilterTextBox.Text;
+            DateTime? date = dateFilterDatePicker.SelectedDate;
+
+            bool applyNameFilter = !string.IsNullOrWhiteSpace(name) && !name.Equals(NameSearchText);
+            bool applyDateFilter = (date != null);
+
+            if (applyNameFilter || applyDateFilter)
+            {
+                list = list.Where(x => (applyNameFilter ? x.Name.Contains(name) : true) &&
+                    (applyDateFilter ? (DateTime.Compare(x.LastExamineDate.Date, date.Value.Date) == 0) : true));
+            }
+
+            /*if (!string.IsNullOrWhiteSpace(name) && !name.Equals(NameSearchText))
+            {
+                list = list.Where(x => x.Name.Contains(name));
+            }*/
+
+            //if (date != null)
+            //{
+            //    list = list.Where(x => x.LastExamineDate.Date == ((DateTime)date).Date) as List<TablePatient>;
+            //}
+
+            patientsGrid.ItemsSource = list;
+            //MessageBox.Show('')
+        }
+
+        private void SetDatePickerText(DatePicker dp, string text = null)
+        {
+            DatePickerTextBox datePickerTextBox = FindVisualChild<DatePickerTextBox>(dp);
+            if (datePickerTextBox != null)
+            {
+                ContentControl watermark = datePickerTextBox.Template.FindName("PART_Watermark", datePickerTextBox) as ContentControl;
+                if (watermark != null)
+                {
+                    watermark.Content = text == null ? DateSearchText : text;
+                }
+            }
         }
 
         private void ShowHideDetails(object sender, RoutedEventArgs e)
@@ -49,7 +97,9 @@ namespace Client
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            var patients = PatientsRepo.GridList();
+            nameFilterTextBox.Text = NameSearchText;
+            
+            patients = PatientsRepo.GridList();
 
             if (patients == null || !patients.Any())
             {
@@ -57,30 +107,76 @@ namespace Client
             }
 
             patientsGrid.ItemsSource = patients;
+        }
 
-            //DataTable patientsTable = new DataTable();
+        private void nameFilterTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            RefreshFilter();
+        }
 
-            //MongoRepository<Examine> examines = new MongoRepository<Examine>();
+        private void clearNameFilterButton_Click(object sender, RoutedEventArgs e)
+        {
+            nameFilterTextBox.Text = NameSearchText;
+        }
 
-            //List<Patient> patients = new PgContext().Patients.OrderByDescending(p => p.Id).ToList();       
-            //List<int> patientsIds = patients.Select(p => p.Id).ToList();                        
-            //List<Examine> examinesPool = examines.Where(ex => patientsIds.Contains(ex.PatientId))
-            //    .OrderByDescending(ex => ex.CreatedAt)
-            //    .ToList();
+        private void nameFilterTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (nameFilterTextBox.Text.Equals(NameSearchText))
+            { 
+                nameFilterTextBox.Text = "";
+            }
+        }
 
-            //List<TablePatient> tablePatients = new List<TablePatient>();
-            //foreach (Patient patient in patients)
-            //{
-            //    var patientExamines = examinesPool.Where(ex => ex.PatientId == patient.Id).ToList();
+        private void nameFilterTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (nameFilterTextBox.Text.Length == 0)
+            {
+                nameFilterTextBox.Text = NameSearchText;
+            }
+        }
 
-            //    if (patientExamines.Any())
-            //    {
-            //        var examine = patientExamines.First();
-            //        var tp = new TablePatient(patient, examine);
+        private void dateFilterDatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            RefreshFilter();
+        }
 
-            //        tablePatients.Add(tp);
-            //    }
-            //}
+        private void clearDateFilterButton_Click(object sender, RoutedEventArgs e)
+        {
+            dateFilterDatePicker.SelectedDate = null;
+            SetDatePickerText(dateFilterDatePicker);
+        }
+
+        private void patientsGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            nameFilterTextBox.IsEnabled = true;
+            dateFilterDatePicker.IsEnabled = true;
+            clearNameFilterButton.IsEnabled = true;
+            clearDateFilterButton.IsEnabled = true;
+        }
+
+        private void dateFilterDatePicker_Loaded(object sender, RoutedEventArgs e)
+        {
+            DatePicker datePicker = sender as DatePicker;
+            if (datePicker != null)
+            {
+                SetDatePickerText(datePicker);
+            }
+        }
+
+        private T FindVisualChild<T>(DependencyObject depencencyObject) where T : DependencyObject
+        {
+            if (depencencyObject != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depencencyObject); ++i)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depencencyObject, i);
+                    T result = (child as T) ?? FindVisualChild<T>(child);
+                    if (result != null)
+                        return result;
+                }
+            }
+
+            return null;
         }
     }
 }
